878612991e6aa4c307caf96d27032a767d3e7e2e
usman <usmanaamirbs2022@gmail.com>
2024-08-25 14:49:18 +0000

    q2 completed

--- q2.py
+++ q2.py
@@ -5,4 +5,5 @@ def areaCalculator(length:float,width:float)->float:
 
 length = float(input("Enter the length of rectangle: "))
 width = float(input("Enter the width of rectangle: "))
-print("Area of rectangle is",areaCalculator(length,width))
+rectangle = areaCalculator(length,width)
+print("Area of rectangle is",rectangle)
