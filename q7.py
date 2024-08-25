c3cd2ac7ff707c12a0c7563b84f3482318394c53
usman <usmanaamirbs2022@gmail.com>
2024-08-25 15:02:13 +0000

    q7 completed

--- q7.py
+++ q7.py
@@ -1,7 +1,8 @@
-def calPercentage(numerator:float , denominator:float)->float
+def calPercentage(numerator:float , denominator:float)->float:
     percentage : float= (numerator / denominator) * 100
     return percentage
 
 numerator = float(input("Enter the numerator value: "))
 denominator = float(input("Enter the denominator value: "))
-print(numerator,"is", calPercentage(numerator,denominator),"%" ,"of", denominator)
+percenage : float = calPercentage(numerator,denominator)
+print(numerator,"is", percenage,"%" ,"of", denominator)
